bb9debf70ee98169ef64bcc1ef75ac8e17d2eb2e
Abner <abnero@gmail.com>
2017-08-05 22:17:35 -0700

    added music, custom fonts, collection view

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,34 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Abner on 8/4/17.
+//  Copyright © 2017 Abner. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    //rounded corners for the cell
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(_ pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+}
---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,30 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Abner on 8/4/17.
+//  Copyright © 2017 Abner. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    fileprivate var _name: String!
+    fileprivate var _pokedexId: Int!
+    
+    var name: String {
+        
+        return _name
+    }
+    
+    var pokedexId: Int {
+        
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
